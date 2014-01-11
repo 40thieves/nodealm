dde86cfd23a60c6a4cf6c77202a2fb0e550a8362
Alasdair Smith <alasdairsmith100@gmail.com>
2014-01-11 00:11:48 +0000

    CLI working, logs out metadata and summary altmetrics

--- app.js
+++ app.js
@@ -2,17 +2,28 @@
 
 var alm = require('./lib/nodeAlm')
 ,	tags = require('./lib/flagParser')
+,	tagDefaults = {}
+,	tagReplacements
+,	options
 ;
 
-var tagDefaults = {};
-
-var tagReplacements = {
+tagReplacements = {
 	i: 'info',
 	d: 'days',
 	m: 'months',
 	y: 'year'
 };
 
-var options = tags.parse(process.argv, tagDefaults, tagReplacements);
+options = tags.parse(process.argv, tagDefaults, tagReplacements);
 
-console.log(options);
+alm.getAlm(options.ids, { info: 'summary' }, function(err, result) {
+	console.log('Metadata');
+	console.log('DOI:', result[0].doi);
+	console.log('Title: ', result[0].title);
+	console.log('URL: ', result[0].url);
+	console.log('\nSummary altmetrics');
+	console.log('Views: ', result[0].views);
+	console.log('Shares: ', result[0].shares);
+	console.log('Bookmarks: ', result[0].bookmarks);
+	console.log('Citations: ', result[0].citations);
+});
