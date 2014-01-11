ab05dc700ab7d72fa9e6e00b85d29dfd3f465361
Alasdair Smith <alasdairsmith100@gmail.com>
2014-01-11 19:27:02 +0000

    Cli handles multiple results

--- cli.js
+++ cli.js
@@ -17,13 +17,27 @@ tagReplacements = {
 options = tags.parse(process.argv, tagDefaults, tagReplacements);
 
 alm.getAlm(options.ids, { info: 'summary' }, function(err, result) {
+	if (typeof result === 'object' && result instanceof Array) {
+		// Result is an array - loop through each
+		result.forEach(function(r) {
+			print(r);
+		});
+	}
+	else {
+		print(result);
+	}
+});
+
+function print(result) {
 	console.log('Metadata');
 	console.log('DOI:', result.doi);
 	console.log('Title: ', result.title);
 	console.log('URL: ', result.url);
+
 	console.log('\nSummary altmetrics');
 	console.log('Views: ', result.views);
 	console.log('Shares: ', result.shares);
 	console.log('Bookmarks: ', result.bookmarks);
 	console.log('Citations: ', result.citations);
-});
+	console.log('\n');
+}
